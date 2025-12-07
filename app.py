e9d77b83eff98f9a88b47020b6d38500c99b0d65
Chamoda De Silva <78342002+ChamodaDSilva@users.noreply.github.com>
2025-12-07 13:59:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -318,5 +318,4 @@ def match_performance(match_id):
     return render_template('match_performance.html', match=match, players=players, performances_map=performances_map)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
